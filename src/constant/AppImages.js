119f4deccb87bec8355b375795ae2c4246c2c604
Usman-99 <usmanyousuf1137@gmail.com>
2025-01-01 11:10:47 +0500

    making object for image paths

---
+++ src/constant/AppImages.js
@@ -0,0 +1,12 @@
+
+export const allAppImages={
+    avatar2: "./assets/avatars/2.jpg",
+    avatar1: "./assets/avatars/1.jpg",
+    avatar3: "./assets/avatars/3.jpg",
+    avatar4: "./assets/avatars/4.jpg",
+    avatar5: "./assets/avatars/5.jpg",
+    avatar6: "./assets/avatars/6.jpg",
+    avatar7: "./assets/avatars/7.jpg",
+    avatar8: "./assets/avatars/8.jpg",
+    logo:"./assets/logo1.png"
+}
